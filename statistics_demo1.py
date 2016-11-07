2148d58441f03e95ea1106a7f18b16869f462483
sg-xue <sg_xue2004@hotmail.com>
2016-11-07 23:04:45 +0800

    draw a bar chart

--- statistics_demo1.py
+++ statistics_demo1.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from numpy import array, mean
-from numpy.random import normal
+import numpy as np
+# from numpy.random import normal
+from matplotlib import pyplot
 
 def genData():
     heights = []
@@ -13,23 +14,54 @@ def genData():
     for i in range(N):
         while True:
             #身高服从均值172，标准差为6的正态分布
-            height = normal(172, 6)
+            height = np.random.normal(172, 6)
             if 0 < height: break
         while True:
             #体重由身高作为自变量的线性回归模型产生，误差服从标准正态分布
-            weight = (height - 80) * 0.7 + normal(0, 1)
+            weight = (height - 80) * 0.7 + np.random.normal(0, 1)
             if 0 < weight: break
         while True:
             #分数服从均值为70，标准差为15的正态分布
-            score = normal(70, 15)
+            score = np.random.normal(70, 15)
             if 0 <= score and score <= 100:
                 grade = 'E' if score < 60 else ('D' if score < 70 else ('C' if score < 80 else ('B' if score < 90 else 'A')))
                 break
         heights.append(height)
         weights.append(weight)
         grades.append(grade)
-    return array(heights), array(weights), array(grades)
+    return np.array(heights), np.array(weights), np.array(grades)
 
 #heights, weights, grades = genData()
-data = normal(0, 3, size=(4,2))
-print data
+
+#绘制柱状图
+def drawBar(grades):
+    xticks = ['A', 'B', 'C', 'D', 'E']
+    gradeGroup = {}
+    #对每一类成绩进行频数统计
+    for grade in grades:
+        gradeGroup[grade] = gradeGroup.get(grade, 0) + 1
+    #创建柱状图
+    #第一个参数为柱的横坐标
+    #第二个参数为柱的高度
+    #参数align为柱的对齐方式，以第一个参数为参考标准
+    pyplot.bar(range(5), [gradeGroup.get(xtick, 0) for xtick in xticks], align='center')
+
+    #设置柱的文字说明
+    #第一个参数为文字说明的横坐标
+    #第二个参数为文字说明的内容
+    pyplot.xticks(range(5), xticks)
+
+    #设置横坐标的文字说明
+    pyplot.xlabel('Grade')
+    #设置纵坐标的文字说明
+    pyplot.ylabel('Frequency')
+    #设置标题
+    pyplot.title('Grades Of Male Students')
+    #绘图
+    pyplot.show()
+
+#drawBar(grades)
+
+if __name__ == "__main__":
+    heights, weights, grades = genData()
+    drawBar(grades)
